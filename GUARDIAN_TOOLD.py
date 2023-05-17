9485d52a8c96d9fb4868ca7c2b3638409b775b4c
GOOD-OVERLEYIN <133844387+GOOD-OVERLEYIN@users.noreply.github.com>
2023-05-17 18:48:11 +0700

    Update GUARDIAN_TOOLD.py

--- GUARDIAN_TOOLD.py
+++ GUARDIAN_TOOLD.py
@@ -47,7 +47,7 @@ while True:
      port = port + 1
      print ("Sent %s packet to %s throught port:%s")%(sent,ip,port)
      if bytes == 65534:
-       bytes = 1
-       elif port == 65534:
-       	port = 1
+        bytes = 1
+        port == 65534:
+        port = 1
 
